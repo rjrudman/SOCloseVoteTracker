15422429184b612fcb6b861a198dda68c6c58fa5
Robert Rudman <robert_rudman@edgeloyalty.com.au>
2016-03-23 15:52:47 +1100

    Get the scaffolding done

---
+++ Data/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.Entity;
+using System.Data.SqlClient;
+
+namespace Data
+{
+    public class DataContext : DbContext
+    {
+        private const string CONNECTION_STRING_NAME = "DBConnectionString";
+
+        /// <summary>
+        /// Get a plain connection to the database. Can be used with dapper. Uses the same connection string as EntityFramework
+        /// </summary>
+        /// <returns></returns>
+        public static IDbConnection PlainConnection()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings[CONNECTION_STRING_NAME].ConnectionString;
+            return new SqlConnection(connectionString);
+        }
+
+        public DataContext() : base("DBConnectionString") { }
+    }
+}
---
+++ Data/Migrations/Configuration.cs
@@ -0,0 +1,16 @@
+namespace Data.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DataContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Data.DataContext context)
+        {
+        }
+    }
+}
