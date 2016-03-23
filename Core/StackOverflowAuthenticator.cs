b32bb3ea1135f43101ea55645fd599f44da51b1d
Robert Rudman <rjrudman@gmail.com>
2016-03-23 19:51:01 +1100

    #1 Refactor authentication

---
+++ Core/StackOverflowAuthenticator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RestSharp;
+using Utils;
+
+namespace Core
+{
+    class StackOverflowAuthenticator
+    {
+        private const string BaseUrl = @"https://stackoverflow.com";
+
+        private DateTime _refreshCookieTime = DateTime.MinValue;
+        private readonly List<RestResponseCookie> _authCookies = new List<RestResponseCookie>();
+
+        private bool CurrentAuthCookiesValid() =>_refreshCookieTime > DateTime.Now;
+
+        private IEnumerable<RestResponseCookie> GetAuthCookies()
+        {
+            if (!CurrentAuthCookiesValid())
+            {
+                var restClient = new RestClient(BaseUrl);
+                var restRequest = new RestRequest("users/login", Method.POST);
+                restClient.FollowRedirects = false;
+
+                restRequest.AddParameter("email", Configuration.UserName);
+                restRequest.AddParameter("password", Configuration.Password);
+
+                var response = restClient.Execute(restRequest);
+
+                _authCookies.AddRange(response.Cookies);
+                _refreshCookieTime = _authCookies.Min(ac => ac.Expires);
+
+            }
+            return _authCookies;
+        }
+
+        public void AuthenticateRequest(IRestRequest request)
+        {
+            foreach (var cookie in GetAuthCookies())
+                request.AddCookie(cookie.Name, cookie.Value);
+        }
+        
+    }
+}
--- SOCloseVoteTracker/Program.cs
+++ SOCloseVoteTracker/Program.cs
@@ -1,59 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Linq;
-using System.Net;
-using System.Runtime.Remoting;
-using System.Text;
-using System.Web;
-using RestSharp;
-using Utils;
-
-namespace SOCloseVoteTracker
+﻿namespace SOCloseVoteTracker
 {
     class Program
     {
         private const string BaseURL = @"https://stackoverflow.com";
         static void Main(string[] args)
         {
-            GetRecentlyClosed();
+            //GetRecentlyClosed();
         }
 
-        private static List<RestResponseCookie> AuthCookies = new List<RestResponseCookie>();
-
-        private static bool RequiresLogin()
-        {
-            return !AuthCookies.Any() || AuthCookies.Any(ac => ac.Expired);
-        }
-
-        static void Login()
-        {
-            var restClient = new RestClient(BaseURL);
-            var restRequest = new RestRequest("users/login", Method.POST);
-            restRequest.AddParameter("email", Configuration.UserName);
-            restRequest.AddParameter("password", Configuration.Password);
-            restClient.FollowRedirects = false;
-            var response = restClient.Execute(restRequest);
-            AuthCookies.AddRange(response.Cookies);
-        }
         
-        static void GetRecentlyClosed()
-        {
-            if (RequiresLogin())
-                Login();
-            
-            var restClient = new RestClient(BaseURL);
-            var restRequest = new RestRequest("tools", Method.GET);
-            restRequest.AddParameter("tab", "close");
-            restRequest.AddParameter("daterange", "today");
-            restRequest.AddParameter("mode", "recentlyClosed");
-
-            foreach (var authCookie in AuthCookies)
-                restRequest.AddCookie(authCookie.Name, authCookie.Value);
-
-            restRequest.AddHeader("X-Requested-With", "XMLHttpRequest");
-
-            var response = restClient.Execute(restRequest);
-        }
     }
 }
