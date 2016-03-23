f815457b060a37c8bc178557f8c16168e4d6a3e8
Robert Rudman <robert_rudman@edgeloyalty.com.au>
2016-03-23 15:44:04 +1100

    Proof of concept done

--- SOCloseVoteTracker/Program.cs
+++ SOCloseVoteTracker/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace SOCloseVoteTracker
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Utils;
+
+namespace SOCloseVoteTracker
 {
     class Program
     {
         static void Main(string[] args)
         {
+            GetRecentlyClosed();
+        }
+
+        static void GetRecentlyClosed()
+        {
+            var endPoint = @"http://stackoverflow.com/tools?tab=close&daterange=today&mode=recentlyClosed";
+            var client = new WebClient();
+
+            client.Headers.Add(HttpRequestHeader.Cookie, $"acct={Configuration.AuthKey}");
+            client.Headers.Add("X-Requested-With","XMLHttpRequest"); //Otherwise we get a full page
+
+            var data = client.DownloadData(endPoint);
+            var stringData = Encoding.UTF8.GetString(data);
         }
     }
 }
---
+++ Utils/Configuration.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace Utils
+{
+    public static class Configuration
+    {
+        private static readonly string SettingsFileLocation = ConfigurationManager.AppSettings["RootDirectory"]  + "settings.json";
+
+        /// <summary>
+        /// Configuration is loaded once at startup and cached
+        /// </summary>
+        static Configuration()
+        {
+            if (!File.Exists(SettingsFileLocation))
+                throw new Exception($"Settings file does not exist: {Path.GetFullPath(SettingsFileLocation)}");
+
+            var data = File.ReadAllText(SettingsFileLocation);
+            var settings = JsonConvert.DeserializeObject<Dictionary<string, object>>(data);
+
+            var props = typeof(Configuration).GetProperties().ToDictionary(p => p.Name, p => p, StringComparer.InvariantCultureIgnoreCase);
+            foreach (var kvp in settings)
+            {
+                if (!props.ContainsKey(kvp.Key))
+                    continue;
+
+                var prop = props[kvp.Key];
+                prop.SetValue(null, kvp.Value);
+            }
+        }
+
+#if DEBUG
+        public static string DefaultConfigurationFile => JsonConvert.SerializeObject(typeof(Configuration).GetProperties().Where(p => p.Name != nameof(DefaultConfigurationFile)).ToDictionary(p => p.Name, p => p.GetValue(null)), Formatting.Indented);
+#endif
+
+        // ReSharper disable once UnusedAutoPropertyAccessor.Local
+        public static string AuthKey { get; private set; }
+        
+    }
+}
