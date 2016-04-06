692340ec381364ffb567ba653d25fd59e67a6b45
Robert Rudman <rjrudman@gmail.com>
2016-04-06 22:04:42 +1000

    Cleanup data

---
+++ Data/Migrations/201604061204218_CleanupExistingData.cs
@@ -0,0 +1,26 @@
+namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CleanupExistingData : DbMigration
+    {
+        public override void Up()
+        {
+            Sql(@"
+DELETE CloseVotes
+FROM CloseVotes
+INNER JOIN Questions on QuestionID = ID
+WHERE Closed = 1
+
+UPDATE Questions SET DeleteVotes = 0 WHERE Deleted = 1
+UPDATE Questions Set UndeleteVotes = 0 WHERE Deleted = 0
+UPDATE Questions SET ReopenVotes = 0 WHERE Closed = 0
+");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
