c8dedb08b4b6b5d79a9f9fd408ee28a28cf17860
Robert Rudman <robert_rudman@edgeloyalty.com.au>
2016-04-08 13:31:56 +1000

    Start tracking logs

---
+++ Data/Migrations/201604080331325_StartLogging.cs
@@ -0,0 +1,28 @@
+namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class StartLogging : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Logs",
+                c => new
+                    {
+                        LogId = c.Int(nullable: false, identity: true),
+                        DateLogged = c.DateTime(nullable: false),
+                        Message = c.String(),
+                        Level = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.LogId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Logs");
+        }
+    }
+}
